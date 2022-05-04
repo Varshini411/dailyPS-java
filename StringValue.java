4a0f669d83a914a4e7988a2d9b3a3a559a9c6743
Varshini411 <kvarshinibecse@gmail.com>
2022-05-04 07:52:53 -0700

    StringValue program

---
+++ StringValue.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class StringValue{  
+public static void main(String args[]){ 
+    Scanner sc = new Scanner(System.in);
+    int n=sc.nextInt();
+String s1=String.valueOf(n);  
+System.out.println(s1+n); 
+}
+}
