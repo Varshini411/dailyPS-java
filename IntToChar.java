64c58247e8445e8fb47378cec313e4e6972284d6
Varshini411 <kvarshinibecse@gmail.com>
2022-05-09 09:40:28 -0700

    IntToChar program added

---
+++ IntToChar.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class IntToChar
+{
+  public static void main (String[]args)
+  {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    char c = (char)a;
+    System.out.println(a);
+  }
+}
